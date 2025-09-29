c76bcc0749ae2df3065c88ffcb6f627365c38361
Basanagoud <basanagoudp329@gmail.com>
2025-09-29 10:48:58 +0530

    Initial commit for TaskMaster

---
+++ TaskMaster/src/test/java/com/project/TaskMaster/TaskMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.TaskMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
